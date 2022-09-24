b7ff06d4b0cacc35cab2d35fb276117245118084
Naveen <nooobimplementsjs@gmail.com>
2022-09-24 12:36:20 +0530

    adding sample code

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use latex2mathml::{latex_to_mathml, DisplayStyle};
 
 fn main() {
     let latex = r#"x^2"#;
-    let latex2 = r#"x*2 + 5x -3 = 2*x + 5"#;
+    let latex2 = r#"x^2 + 5x -3 = 2x + 5"#;
     let mathml = latex_to_mathml(latex, DisplayStyle::Block).unwrap();
     let mathml2 = latex_to_mathml(latex2, DisplayStyle::Block).unwrap();
     println!("{}, {}", mathml, mathml2);
